cb1633ad9b6610bde6dda739f0a794d62cd348cc
Bryggel <35433224+Bryggel@users.noreply.github.com>
2019-11-02 15:55:10 +0100

    Add files via upload

---
+++ Astrotest.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix ='/';
+const fs = require('fs')
+
+
+
+
+
+client.on('ready', () => {
+        console.log ('Le bot marche');
+})
+
+client.on('message',message => 
+{
+
+          if (message.content === prefix + 'ping') 
+          {
+            message.channel.send ('Je suis la création de Bryggel');
+          }
+
+
+
+
+
+})
+
+
+
+client.login("NjM5ODAxMjc1NTIwNzc4MjYw.XbwyLg._PDw6s867tHocnNuc1rdnC0JL70");
